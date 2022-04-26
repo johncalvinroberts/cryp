e65d8b2417adf356b23f903a9e81d93289c86499
John Roberts <john.calvin.roberts@gmail.com>
2022-04-26 12:03:29 +0800

    fix: remove logger from worker

--- src/lib/crypto.worker.ts
+++ src/lib/crypto.worker.ts
@@ -1,4 +1,3 @@
-import Guu from "guu";
 import { encrypt, decrypt, hexEncode, hexDecode } from "./crypto";
 import { MESSAGE, STATE } from "./constants";
 import { formatCrypString } from "./utils";
@@ -7,13 +6,10 @@ import type { MessagePayload, HexEncodedFile, EncrypterState } from "./types";
 // alias self to ctx and give it our newly created type
 const ctx: Worker = self as any;
 
-const log = new Guu("crypto.worker", "#6a11cb");
-
 // main class wrapper for the worker
 class CryptoWorker {
   encrypt = async (encrypterState: EncrypterState) => {
     try {
-      log.info("Encrypting");
       const { filesToEncrypt, password, hint } = encrypterState;
       const accepted = await Promise.all(
         filesToEncrypt.accepted.map((item) => item.arrayBuffer())
@@ -25,11 +21,9 @@ class CryptoWorker {
       // the plaintext is a stringified JSON array of files
       const plaintext = JSON.stringify(hexEncodedFiles);
       const ciphertext = await encrypt(password, plaintext);
-      log.info("Encrypted");
       const crypString = formatCrypString(ciphertext, hint);
       const payload = { ciphertext, crypString, state: STATE.DONE };
       ctx.postMessage({ payload, type: MESSAGE.ENCRYPTED });
-      log.info("Posted message back to main thread");
     } catch (error) {
       const payload = {
         state: STATE.FAILURE,
@@ -41,20 +35,16 @@ class CryptoWorker {
 
   decrypt = async (encrypterState: EncrypterState) => {
     try {
-      log.info("Decrypting");
       const { ciphertext, password } = encrypterState;
       const plaintext = await decrypt(password, ciphertext);
-      log.info("Decrypted");
       const hexEncodedFiles: HexEncodedFile[] = JSON.parse(plaintext);
       const decryptedFiles = hexEncodedFiles.map((item) => {
         const blob = new Blob([hexDecode(item.hex)]);
         return new File([blob], item.name);
       });
       const payload = { decryptedFiles, state: STATE.DONE };
-      log.info("Posted message back to main thread");
       ctx.postMessage({ payload, type: MESSAGE.DECRYPTED });
     } catch (error) {
-      log.error(error);
       ctx.postMessage({
         state: STATE.FAILURE,
         error,
@@ -64,7 +54,6 @@ class CryptoWorker {
 
   handleMessage = (msg: MessageEvent<MessagePayload>) => {
     const { type, payload } = msg.data;
-    log.info(msg.data);
     switch (type) {
       case MESSAGE.ENCRYPT:
         this.encrypt(payload);
@@ -79,7 +68,6 @@ class CryptoWorker {
 }
 
 // instantiate a worker
-log.info("Instantiating worker");
 const cryptoWorker = new CryptoWorker();
 // add listener to the worker global scope
 ctx.addEventListener("message", cryptoWorker.handleMessage);
