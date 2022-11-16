d0f18eed7d2caad8003638eba251dc442ea9bb37
John Roberts <john.calvin.roberts@gmail.com>
2022-11-16 02:17:55 -0500

    fix: script

--- scripts/ensure-git-clean.sh
+++ scripts/ensure-git-clean.sh
@@ -2,6 +2,7 @@
 
 if [ -n "$(git status --porcelain)" ]; then
   echo "Git status is not clean after installing and linting. Please double check lockfiles and your code formatting.";
+  git status
   exit 1;
 else
   echo "No changes detected after running i18n string extractor";
