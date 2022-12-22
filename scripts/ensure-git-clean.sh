babfede2f8ca62706f50d9909ee69fd630111105
John Roberts <john.calvin.roberts@gmail.com>
2022-12-21 22:12:21 -0600

    fix: pnpm gh action

--- scripts/ensure-git-clean.sh
+++ scripts/ensure-git-clean.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# hate that i have to do this, but the gh action pnpm/action-setup@v2 alters pnpm-lock.yaml
+git checkout ui/pnpm-lock.yaml
+
 if [ -n "$(git status --porcelain)" ]; then
   echo "Git status is not clean after installing and linting. Please double check lockfiles and your code formatting.";
   git status
-  git diff ui/pnpm-lock.yaml
   exit 1;
 else
   echo "No changes detected. You r gud.";
