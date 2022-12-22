910a1fd98192eeaf898d797b96746fc464bc2747
John Roberts <john.calvin.roberts@gmail.com>
2022-12-21 21:29:25 -0600

    fix: gh actions

--- scripts/ensure-git-clean.sh
+++ scripts/ensure-git-clean.sh
@@ -3,7 +3,8 @@
 if [ -n "$(git status --porcelain)" ]; then
   echo "Git status is not clean after installing and linting. Please double check lockfiles and your code formatting.";
   git status
+  git diff ui/pnpm-lock.yaml
   exit 1;
 else
-  echo "No changes detected after running i18n string extractor";
+  echo "No changes detected. You r gud.";
 fi
